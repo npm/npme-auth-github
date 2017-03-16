c66cb29a7df5ab0eb84cf11604f831fcd33b3fc4
nanovazquez <marianodvazquez@gmail.com>
2017-03-15 22:23:34 -0300

    fix(authorizer): Fix organization check when multi-org is configured

--- test/authorize-test.js
+++ test/authorize-test.js
@@ -120,7 +120,7 @@ Lab.experiment('loadPackageJSON', function() {
     });
   });
 
-  Lab.test.only('gracefully handles request returning an error', function(done) {
+  Lab.test('gracefully handles request returning an error', function(done) {
     nock.cleanAll();
 
     var ga = new AuthorizeGithub({
@@ -388,6 +388,40 @@ Lab.experiment('isAuthorized', function() {
       token: 'banana',
       scope: 'publish',
       githubOrg: 'npm-test',
+      debug: false
+    });
+
+    ga.isAuthorized().done(function(authorized) {
+      assert(authorized);
+
+      packageApi.done();
+      githubApi.done();
+      done();
+    });
+  });
+
+  Lab.test('authorization succeeds on publish, if git-URL-org matches any githubOrg', function(done) {
+
+    // HTTP response for loading package.json
+    var packageApi = nock('http://frontdoor.npmjs.com')
+      .get('/@npm-test/foo?sharedFetchSecret=' + config.sharedFetchSecret)
+      .reply(200, JSON.stringify({
+        repository: { url: 'http://github.npmjs.com/npm-test/foo.git' }
+      }));
+
+    // HTTP response for use with read/write permissions on repo.
+    var githubApi = nock('https://github.example.com')
+      .get('/api/v3/repos/npm-test/foo?access_token=banana')
+      .reply(200, fs.readFileSync('./test/fixtures/read-write.json'), {
+        'content-type': 'application/json; charset=utf-8'
+      })
+
+    var ga = new AuthorizeGithub({
+      frontDoorHost: 'http://frontdoor.npmjs.com',
+      packagePath: '/@npm-test/foo',
+      token: 'banana',
+      scope: 'publish',
+      githubOrg: 'otherOrg, npm-test, org3',
       debug: false
     });
 
